e4f5ef482730e08ae4dca990af9a7900ba73e1f4
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2020-05-07 12:12:44 +0100

    Update VendingLootDrop.cs

--- VendingLootDrop.cs
+++ VendingLootDrop.cs
@@ -1,15 +1,9 @@
-using UnityEngine;
-
 namespace Oxide.Plugins
 {
     [Info("Vending Loot Drop", "Bazz3l", "1.0.2")]
     [Description("Drops vending machine loot when destroyed.")]
     class VendingLootDrop : RustPlugin
     {
-        #region Fields
-        const string _itemPrefab = "assets/prefabs/misc/item drop/item_drop.prefab";
-        #endregion
-
         #region Oxide
         void OnEntityDeath(VendingMachine machine)
         {
@@ -18,10 +12,7 @@ namespace Oxide.Plugins
                 return;
             }
 
-            if (machine.inventory.itemList.Count > 0)
-            {
-                machine.inventory.Drop(_itemPrefab, machine.ServerPosition + (Vector3.up * 0.3f), machine.ServerRotation);
-            }
+            machine.dropChance = 0.75f;
         }
         #endregion
     }
