edbfaed96ef92eb6e6cca15d0a40d292c69c63a6
Bazz3l <30571705+bazz3l@users.noreply.github.com>
2020-05-22 16:01:24 +0100

    Fix for already placed vending machines

--- VendingLootDrop.cs
+++ VendingLootDrop.cs
@@ -1,16 +1,54 @@
+using Newtonsoft.Json;
+using System.Linq;
+
 namespace Oxide.Plugins
 {
-    [Info("Vending Loot Drop", "Bazz3l", "1.0.2")]
-    [Description("Drops vending machine loot when destroyed.")]
+    [Info("Vending Loot Drop", "Bazz3l", "1.0.5")]
+    [Description("Drops vending machine contents when destroyed.")]
     class VendingLootDrop : RustPlugin
     {
+        #region Fields
+        PluginConfig _config;
+        #endregion
+
+        #region Config
+        PluginConfig GetDefaultConfig()
+        {
+            return new PluginConfig {
+                DropChance = 0.75f
+            };
+        }
+
+        class PluginConfig
+        {
+            [JsonProperty(PropertyName = "Drop chance (percents, 1f = 100%)")]
+            public float DropChance;
+        }
+        #endregion
+
         #region Oxide
+        protected override void LoadDefaultConfig() => Config.WriteObject(GetDefaultConfig(), true);
+
+        private void OnServerInitialized()
+        {
+            Subscribe(nameof(OnEntitySpawned));
+
+            foreach (VendingMachine machine in BaseNetworkable.serverEntities.OfType<VendingMachine>())
+            {
+                OnEntitySpawned(machine);
+            }
+        }
+
+        void Init()
+        {
+            _config = Config.ReadObject<PluginConfig>();
+
+            Unsubscribe(nameof(OnEntitySpawned));
+        }
+
         void OnEntitySpawned(VendingMachine machine)
         {
-            if (machine != null)
-            {
-                machine.dropChance = 0.75f;
-            }
+            machine.dropChance = _config.DropChance;
         }
         #endregion
     }
